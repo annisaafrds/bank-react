6556bc9529c1b04cafd64f65936483f6eb897cad
dwimh <dwimegah.dm@gmail.com>
2023-03-13 14:29:25 +0700

    Tampilan menu setor tunai

--- ui/src/views/nasabah/setor-tunai/Setor.js
+++ ui/src/views/nasabah/setor-tunai/Setor.js
@@ -3,25 +3,45 @@ import {
   CButton,
   CCard,
   CCardBody,
+  CCardFooter,
   CCardHeader,
   CCol,
-  CForm,
   CFormInput,
-  CFormLabel,
-  CFormTextarea,
   CRow,
 } from '@coreui/react'
-import { DocsExample } from 'src/components'
 
 const setor = () => {
   return (
     <CRow>
       <CCol xs={12}>
-        <CCard>
+        <CCard mb={4}>
           <CCardHeader>
             <strong>Setor Tunai</strong>
           </CCardHeader>
-          <CCardBody>This is some text within a card body.</CCardBody>
+          <CCardBody>
+            <CCol sm={2}>
+              <p className="text-medium-emphasis small">
+                Masukkan Nomor Rekening:
+              </p>
+              <CFormInput
+                  className="col-sm-2"
+                  type="text"
+                  placeholder="Nomor Rekening"
+                  aria-label="default input example"
+              />
+            </CCol>
+          </CCardBody>
+          <CCardFooter>
+            <div>
+              <div>
+                <div>
+                  <button class="btn btn-info" type="button">Submit</button>
+                  &nbsp;
+                  <button class="btn btn-secondary" type="button">Batal</button>&nbsp;
+                </div>
+              </div>
+            </div>
+          </CCardFooter>
         </CCard>
       </CCol>
     </CRow>
