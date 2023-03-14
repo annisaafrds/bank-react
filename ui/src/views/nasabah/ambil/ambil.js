5aa92fb6237bff2a09eccf4ab413375b88af75c7
Reynaldi <reynaldi@gitlab.lab>
2023-03-14 14:12:21 +0700

    update menu ambil tunai

--- ui/src/views/nasabah/ambil/ambil.js
+++ ui/src/views/nasabah/ambil/ambil.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {
   CButton,
   CCard,
@@ -13,50 +13,115 @@ import {
   CCardFooter,
 } from '@coreui/react'
 import { DocsExample } from 'src/components'
+import { url } from '../../../Constanta';
+import ListAmbil from './ListAmbil';
 
-const Ambil = () => {
+class Ambil extends Component {
 
-  // cancelButton() {
-  //   document.getElementById("input-rek").reset();
-  // }
+  constructor(props) {
+    super(props);
+    this.state={
+      norek:'',
+    };
+    this.ubahNorek = this.ubahNorek.bind(this);
+    this.clearNorek = this.clearNorek.bind(this);
+    this.ambilTunai = this.ambilTunai.bind(this);
+    this.listAmbil = this.listAmbil.bind(this);
+  }
 
-  return (
-    <CRow>
-      <CCol xs={12}>
-        <CCard mb={4}>
-          <CCardHeader>
-            <strong>Ambil Tunai</strong>
-          </CCardHeader>
-          <CCardBody>
-            <CCol sm={3}>
-              <form id="input-rek">
-              <p className="text-medium-emphasis small w-full">
-                Masukkan Nomor Rekening:
-              </p>
-              <CFormInput
-                  className="col-sm-2"
-                  type="text"
-                  placeholder="Nomor Rekening"
-                  aria-label="default input example"
-              />
-              </form>
-            </CCol>
-          </CCardBody>
-          <CCardFooter>
-            <div>
+  listAmbil(value) {
+    if(value != '') {
+      fetch(`${url}/api/mst-bank/getByNorek?value=${value}`)
+        .then((response) => response.json())
+        .then((data) => {
+
+        console.log('a', data.rows);
+          this.setState(
+            (prevState) => ({
+              listData: data.rows,
+            }),
+            () => {
+
+            console.log('a', data.rows);
+            // console.log('a', this.state.listTransaksiTelkom);
+
+            }
+          );
+        })
+        .catch((Err) => {
+          alert("Tidak meload data1");
+        });
+      }
+  }
+
+  ambilTunai(e) {
+    e.preventDefault();
+    this.listAmbil(this.state.norek);
+  }
+
+  ubahNorek(e) {
+    console.log("eventUbah:",e.target.value);
+    this.setState(prevState => ({
+      norek: e.target.value
+    }))
+    // console.log("eventUbah2:",this.state.norek);
+  }
+
+  clearNorek(e) {
+    this.setState(prevState => ({
+      norek: ''
+    }))
+  }
+
+  render() {
+    return (
+      <CRow>
+        <CCol xs={12}>
+          <CCard mb={4}>
+            <CCardHeader>
+              <strong>Ambil Tunai</strong>
+            </CCardHeader>
+            <CCardBody>
+              <CCol sm={3}>
+                <p className="text-medium-emphasis small w-full">
+                  Masukkan Nomor Rekening:
+                </p>
+                <CFormInput
+                    value={this.state.norek}
+                    className="col-sm-2"
+                    type="text"
+                    placeholder="Nomor Rekening"
+                    aria-label="default input example"
+                    onChange={this.ubahNorek}
+                />
+              </CCol>
+            </CCardBody>
+            <CCardFooter>
               <div>
                 <div>
-                  <button class="btn btn-info" type="button">Submit</button>
-                  &nbsp;
-                  <button class="btn btn-secondary" type="button">Batal</button>&nbsp;
+                  <div>
+                    <button className="btn btn-info" type="button" onClick={this.ambilTunai}>Ambil</button>
+                    &nbsp;
+                    <button className="btn btn-secondary" type="button" onClick={this.clearNorek}>Batal</button>&nbsp;
+                  </div>
                 </div>
               </div>
-            </div>
-          </CCardFooter>
-        </CCard>
-      </CCol>
-    </CRow>
-  )
+            </CCardFooter>
+          </CCard>
+
+          <ListAmbil
+            listData={this.state.listData}
+            // updateList={this.updateList}
+            // handlePageChange={this.handlePageChange}
+            // handlePerRowsChange={this.handlePerRowsChange}
+            // countPerPage={this.state.size}
+            // // deletePegawai={this.deletePegawai}
+            // totalData={this.state.totalData}
+          />
+        </CCol>
+      </CRow>
+    )
+  }
 }
 
 export default Ambil
