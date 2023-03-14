598a76e21bef56e4b21e27630657a25cb7804889
sakti <saktiprahasdeva@gmail.com>
2023-03-14 13:57:28 +0700

    cek-saldo dan bank-master

--- ui/src/views/nasabah/cek-saldo/cek-saldo.js
+++ ui/src/views/nasabah/cek-saldo/cek-saldo.js
@@ -1,31 +1,116 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import '../../../style.css';
+import DataTable, {
+  createTheme,
+  defaultThemes,
+} from "react-data-table-component";
+// import React from 'react'
 import {
   CButton,
   CCard,
   CCardBody,
+  CCardFooter,
   CCardHeader,
   CCol,
-  CForm,
   CFormInput,
-  CFormLabel,
-  CFormTextarea,
   CRow,
 } from '@coreui/react'
-import { DocsExample } from 'src/components'
 
-const Saldo = () => {
+const setor = () => {
+  const [norek, setNorek] = useState('');
+  const [acountBank, setAcountBank] = useState([]);
+  const [disabled, setDisabled] = useState(true);
+  const columns = [
+        {"name": "alamat",
+          // selector: "HIRE_DATE",
+          cell:(row) => row.alamat
+        },
+        {"name": "nama",
+        // selector: "HIRE_DATE",
+        cell:(row) => row.nama
+      },
+      {"name": "noTelp",
+      // selector: "HIRE_DATE",
+      cell:(row) => row.noTelp
+    },
+    {"name": "norek",
+    // selector: "HIRE_DATE",
+    cell:(row) => row.norek
+  },
+        {
+          'name': "saldo",
+          // selector: "HIRE_DATE",
+          cell:(row) => row.saldo
+        },
+        {
+          'name': "userId",
+          // selector: "HIRE_DATE",
+          cell:(row) => row.userId!==null?row.userId:''
+        },
+      ]
+  const listSaldo = (e) => {
+    alert('listSaldo')
+    e.preventDefault();
+    alert(`http://localhost:3535/api/mst-bank/getByNorek?value=`+norek,)
+    //const product = { title, price };
+  fetch(`http://localhost:3535/api/mst-bank/getByNorek?value=`+norek)
+    .then((response) =>
+     // console.log("response",response)
+      response.json()
+  )
+  .then(json => {
+    //acountBank,
+    alert("json")
+    console.log("json",json)
+    setAcountBank(json)
+  }
+      )
+}
   return (
     <CRow>
       <CCol xs={12}>
-        <CCard>
+        <CCard mb={4}>
           <CCardHeader>
             <strong>Cek Saldo</strong>
           </CCardHeader>
-          <CCardBody>test</CCardBody>
+          <CCardBody>
+            <CCol sm={2}>
+              <p className="text-medium-emphasis small">
+                Masukkan Nomor Rekening:
+              </p>
+              <CFormInput
+                  className="col-sm-5"
+                  type="text"
+                  placeholder="Nomor Rekening"
+                  aria-label="default input example"
+                  onChange={(e) =>
+                    {setNorek(e.target.value)
+                      setDisabled(false)
+                    }}
+              />
+            </CCol>
+          </CCardBody>
+          <CCardFooter>
+            <div>
+              <div>
+                <div>
+                  <button className="btn btn-info" disabled={disabled}  onClick={listSaldo}>Submit</button>
+                  &nbsp;
+                  <button className="btn btn-secondary" >Batal</button>&nbsp;
+<DataTable
+columns={columns}
+          data={acountBank.rows}
+          theme="solarized"
+          selectableRows
+          />
+                </div>
+              </div>
+            </div>
+          </CCardFooter>
         </CCard>
       </CCol>
     </CRow>
   )
 }
 
-export default Saldo
+export default setor
