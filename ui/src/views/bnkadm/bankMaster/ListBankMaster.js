7cf089a303a1962e4c29c6a4cc5caf2b221cfb64
sakti <saktiprahasdeva@gmail.com>
2023-03-14 14:54:16 +0700

    bank master

--- ui/src/views/bnkadm/bankMaster/ListBankMaster.js
+++ ui/src/views/bnkadm/bankMaster/ListBankMaster.js
@@ -1,83 +1,63 @@
 import './App.css';
-import './style.css';
+import '../../../style.css';
 import React, { Component } from 'react';
 import DataTable, {
     createTheme,
-    defaultThemes,} from "react-data-table-component";
+    defaultThemes,
+} from "react-data-table-component";
 import { connect } from "react-redux";
 
-class ListMasterBank extends Component {
+
+class ListBankMaster extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
         const columns = [
-            // {
-            //     name: "Employee Id",
-            //     selector: "ID_TRANSAKSI_NASABAH",
-            // },
             {
-                name: "Norek",
-                selector: "NOREK",
+                name: "NOREK",
+                selector: "norek",
             },
             {
-                name: "Alamat",
-                selector: "ALAMAT",
+                name: "NAMA",
+                selector: "nama",
             },
             {
-                name: "Notlp",
-                selector: "NOTLP",
+                name: "ALAMAT",
+                selector: "alamat",
             },
             {
-                name: "Saldo",
-                selector: "SALDO",
+                name: "NO TELPON",
+                selector: "noTelp",
             },
-            // {
-            //     name: "Status",
-            //     selector: "STATUS_KET",
-            // },
-            // {
-            //     name: "Norek Dituju",
-            //     selector: "NOREK_DITUJU",
-            // },
-            // {
-            //     name: "No",
-            //     selector: "NO_TLP",
-            // },
-            // {
-            //     name: "Department",
-            //     // cell: (row) => row.departments.DEPARTMENT_NAME,
-            //     cell: (row) => row.departments === null ? '' : row.departments.DEPARTMENT_NAME,
-            // },
-            // {
-            //     name: "Job",
-            //     cell: (row) => row.jobs.JOB_TITLE,
-            // },
-            // {
-            //     name: "Action",
-            //     button: true,
-            //     cell: (row) => {
-            //         return (
-            //             <>
-            //                 <button onClick={() => this.props.updateList(row)}>Edit</button>
-            //                 <button onClick={(e) => this.props.deletePegawai(row)}>Delete</button>
-            //             </>
+            {
+                name: "SALDO",
+                selector: "saldo",
+            },
+            {
+              name: "Action",
+              button: true,
+              cell: (row) => {
+                return (
+                  <>
+                  <button onClick={()=>this.props.updateList(row)}>Edit</button>
+                  <button onClick={()=>this.props.deleteList(row)}>Delete</button>
+                  </>
+                );
+              },
+            }
 
-            //         );
-            //     },
-            // },
+
         ];
 
         return (
             <>
-                {/* <p>{this.props.ListMasterBank}</p> */}
+           == {this.props.total_data}
                 <DataTable
                     columns={columns}
-                    data={this.props.listMasterBank}
+                    data={this.props.listBankMaster}
                     theme="solarized"
-                    // customStyles={tableCustomStyle}
-                    // customStyles={customStyles}
                     selectableRows
                     pagination
                     paginationServer
@@ -86,50 +66,11 @@ class ListMasterBank extends Component {
 
                     onChangePage={(page) => this.props.handlePageChange(page)}
                     onChangeRowsPerPage={(size) => this.props.handlePerRowsChange(size)}
-                //firstPage = {this.props.firstPageSage}
-                //nextPage = {()=>alert("sasasa1111")}
-                //onLastPage = {()=>this.props.lastPage}
                 />
             </>
         )
     }
 }
 
-// const mapStateToProps = (state) => {
-//     return {
-//         dataListPegawai: state.listMasterBank,
-//         dataListPegawaiDb: state.listPegawaiDb,
-//         //dataOptionEmp: state.optionEmp,
-//         dataPegawai: state.pegawai,
-//         //dataEmpExcel : state.empExcel,
-//         dataKet: state.ket,
-//         dataField: state.field,
-//         dataValue: state.value,
-//         dataPage: state.page,
-//         dataSize: state.size,
-//         dataNumberOfPage: state.NumberOfPages,
-//         dataTotalData: state.total_data,
-//         dataFirstPage: state.firstPage,
-//         //        dataDEPTNOS: state.DEPTNOS
-//     };
-// };
 
-// const mapDispactToProps = (dispatch) => {
-//     return {
-//         dispatchListPegawai: (listPegawai) =>
-//             dispatch({ type: "UBAH_LIST_PEGAWAI", newValue: listPegawai }),
-//         /*dispatchListPegawaiDb: (listPegawaiDb) =>
-//           dispatch({ type: "UBAH_LIST_PEGAWAI_DB", newValue: listPegawaiDb }),*/
-//         dispatchPegawai: (pegawai) => dispatch({ type: "UBAH_PEGAWAI", newValue: pegawai }),
-//         dispatchKet: (ket) => dispatch({ type: "UBAH_KET", newValue: ket }),
-//         dispatchPage: (page) => dispatch({ type: "UBAH_PAGE", newValue: page }),
-//         dispatchSize: (size) => dispatch({ type: "UBAH_SIZE", newValue: size }),
-//         dispatchNumberOfPage: (NumberOfPages) =>
-//             dispatch({ type: "UBAH_NUMBER_OF_PAGE", newValue: NumberOfPages }),
-//         dispatchTotalData: (totalData) => dispatch({ type: "UBAH_TOTAL_DATA", newValue: totalData })
-//     };
-// };
-
-
-
-export default ListMasterBank;
+export default ListBankMaster;
