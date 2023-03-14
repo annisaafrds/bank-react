63c64f2cbeca41cf23bd74dd2a05c6e7b536e29a
Reynaldi <reynaldi@gitlab.lab>
2023-03-14 15:55:38 +0700

    benerin avatar

--- ui/src/components/header/AppHeaderDropdown.js
+++ ui/src/components/header/AppHeaderDropdown.js
@@ -29,9 +29,9 @@ const AppHeaderDropdown = () => {
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0" caret={false}>
         {/* <CAvatar className='avatar-lg' src={avatar8} size="lg" /> */}
-        <div class='avaatar'>
-          <img class="avatar-img avatar-lg" src={avatar8} alt="user@email.com" />
-          <span class="avatar-status bg-success"></span>
+        <div className='avatar'>
+          <img className="avatar-img avatar-sm" src={avatar8} alt="user@email.com" />
+          <span className="avatar-status bg-success"></span>
         </div>
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
