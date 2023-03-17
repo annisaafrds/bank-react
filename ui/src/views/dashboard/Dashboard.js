43d7325e1561e101fb5588a3fe0ddd9dfbb8b863
Reynaldi <reynaldi@gitlab.lab>
2023-03-17 22:42:20 +0700

    styling

--- ui/src/views/dashboard/Dashboard.js
+++ ui/src/views/dashboard/Dashboard.js
@@ -21,8 +21,9 @@ const Dashboard = () => {
       <div className="grid grid-nogutter surface-0 text-800">
         <div className="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
           <section>
+            <span className="block text-6xl font-bold mb-1 text-left">Hai <b>{localStorage.getItem("nama")}</b></span>
             <span className="block text-6xl font-bold mb-1">Welcome to <span className="text-6xl text-primary font-bold mb-3">Bank Ogya</span></span>
-            <p className="mt-0 mb-4 text-700 line-height-3">Hai <b>{localStorage.getItem("nama")}</b> <br/> ROLE anda: Admin All Role<br/><b>
+            {/* <p className="mt-0 mb-4 text-700 line-height-3">Hai <b>{localStorage.getItem("nama")}</b> <br/> ROLE anda: Admin All Role<br/><b>
 
             {hakaksess.map((hakakses, i) => (
             <>
@@ -33,7 +34,7 @@ const Dashboard = () => {
             ))
             }
 
-              </b></p>
+              </b></p> */}
           </section>
         </div>
         <div className="col-12 md:col-6 overflow-hidden">
--- ui/src/views/pages/login/Login.js
+++ ui/src/views/pages/login/Login.js
@@ -18,11 +18,14 @@ import {
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 import { async } from 'regenerator-runtime'
-
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(false);
+  const [showDialog, setShowDialog] = useState(false);
 
 
   const doLogin = async (e) => {
@@ -40,48 +43,44 @@ const Login = () => {
       },
       body: JSON.stringify(data),
     })
-    .then((response) =>
-    {
-    // alert('anda berhasil login')
-      return  response.json()
-    }
-    // alert('anda berhasil login')
-     // console.log("response",response)
+      .then((response) => {
+        // alert('anda berhasil login')
+        return response.json()
+      }
+        // alert('anda berhasil login')
+        // console.log("response",response)
 
 
-  )
-  .then(json => {
-    //acountBank,
-    console.log("json",json.data.count)
-// alert('qweqwe')
-if (json.data.count > 0)
-{
-    json.data.rows.map((row) =>
-      {
-        localStorage.setItem("nama",row.nama)
-        console.log("row ke-",row)
-        localStorage.setItem('hakakses', JSON.stringify(row.HAKAKSEs));
-        window.location="/"
-        // row.HAKAKSEs.map((hakakses)=>{
-        //   console.log("hakakses ROLE",hakakses.ROLE)
-        //   localStorage.setItem('ROLE', JSON.stringify(hakakses.ROLE));
-        // }
+      )
+      .then(json => {
+        //acountBank,
+        console.log("json", json.data.count)
+        // alert('qweqwe')
+        if (json.data.count > 0) {
+          json.data.rows.map((row) => {
+            localStorage.setItem("nama", row.nama)
+            console.log("row ke-", row)
+            localStorage.setItem('hakakses', JSON.stringify(row.HAKAKSEs));
+            window.location = "/"
+            // row.HAKAKSEs.map((hakakses)=>{
+            //   console.log("hakakses ROLE",hakakses.ROLE)
+            //   localStorage.setItem('ROLE', JSON.stringify(hakakses.ROLE));
+            // }
 
-        // )
+            // )
+          }
+          )
+
+        } else {
+          setShowDialog(true);
+        }
+
+
+
+
+        // setAcountBank(json)
       }
       )
-
-}else{
- alert("Invalid Login BOSS!")
-
-}
-
-
-
-
-    // setAcountBank(json)
-  }
-  )
 
 
     // if (response.ok) {
@@ -92,89 +91,107 @@ if (json.data.count > 0)
     // }
   };
 
+  const onHideDialog = () => {
+    setShowDialog(false);
+  };
+
 
 
 
   return (
-    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
-            <CCardGroup>
-              <CCard className="p-4">
-                <CCardBody>
-                  <CForm>
-                    <h1>Login</h1>
-                    <p className="text-medium-emphasis">Sign In to your account</p>
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username"
-                      onChange={(e) =>
-                        {setUsername(e.target.value)
-                          // setDisabled(false)
-                        }}/>
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        type="password"
-                        placeholder="Password"
-                        autoComplete="current-password"
-                        onChange={(e) =>
-                          {setPassword(e.target.value)
+
+    <>
+      <Dialog
+        visible={showDialog}
+        onHide={onHideDialog}
+        className="text-center"
+      >
+      <Button icon="pi pi-times" className="p-button-rounded p-button-danger p-button-outlined mb-3" aria-label="Cancel" />
+        <h1>Whoops!</h1>
+
+        <p>Something wrong. <br/>
+        Please check your username and password.</p>
+      </Dialog>
+      <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+        <CContainer>
+          <CRow className="justify-content-center">
+            <CCol md={8}>
+              <CCardGroup>
+                <CCard className="p-4">
+                  <CCardBody>
+                    <CForm>
+                      <h1>Login</h1>
+                      <p className="text-medium-emphasis">Sign In to your account</p>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput placeholder="Username" autoComplete="username"
+                          onChange={(e) => {
+                            setUsername(e.target.value)
+                            // setDisabled(false)
+                          }} />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupText>
+                          <CIcon icon={cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                          type="password"
+                          placeholder="Password"
+                          autoComplete="current-password"
+                          onChange={(e) => {
+                            setPassword(e.target.value)
                             // setDisabled(false)
                           }}
-                      />
-                    </CInputGroup>
-                    <CCardFooter>
-            {/* <div>
+                        />
+                      </CInputGroup>
+                      <CCardFooter>
+                        {/* <div>
               <div> */}
-                {/* <div> */}
-                  {/* <button className="btn btn-info"  onClick={doLogin}>Login</button> */}
-                  {/* </div> */}
-                  </CCardFooter>
-                    <CRow>
-                      <CCol xs={6}>
+                        {/* <div> */}
+                        {/* <button className="btn btn-info"  onClick={doLogin}>Login</button> */}
+                        {/* </div> */}
+                      </CCardFooter>
+                      <CRow>
+                        <CCol xs={6}>
 
-                        <CButton color="primary" className="px-4" onClick={doLogin}>
-                          Login
-                        </CButton>
+                          <CButton color="primary" className="px-4" onClick={doLogin}>
+                            Login
+                          </CButton>
 
-                      </CCol>
-                      <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol>
-                    </CRow>
-                  </CForm>
-                </CCardBody>
-              </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
-                <CCardBody className="text-center">
-                  <div>
-                    {/* <h2>Sign up</h2> */}
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                      tempor incididunt ut labore et dolore magna aliqua.
-                    </p>
-                    <Link to="/register">
-                      {/* <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                        </CCol>
+                        <CCol xs={6} className="text-right">
+                          <CButton color="link" className="px-0">
+                            Forgot password?
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                  </CCardBody>
+                </CCard>
+                <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                  <CCardBody className="text-center">
+                    <div>
+                      {/* <h2>Sign up</h2> */}
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua.
+                      </p>
+                      <Link to="/register">
+                        {/* <CButton color="primary" className="mt-3" active tabIndex={-1}>
                         Register Now!
                       </CButton> */}
-                    </Link>
-                  </div>
-                </CCardBody>
-              </CCard>
-            </CCardGroup>
-          </CCol>
-        </CRow>
-      </CContainer>
-    </div>
+                      </Link>
+                    </div>
+                  </CCardBody>
+                </CCard>
+              </CCardGroup>
+            </CCol>
+          </CRow>
+        </CContainer>
+      </div>
+    </>
   )
 }
 
