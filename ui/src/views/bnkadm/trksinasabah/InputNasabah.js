41243e883cc2e2c71d0fc1cdbd4794afef02778a
annisafirdaus <annisa.firdaus@mail.ugm.ac.id>
2023-03-17 09:43:06 +0700

    tambahan

--- ui/src/views/bnkadm/trksinasabah/InputNasabah.js
+++ ui/src/views/bnkadm/trksinasabah/InputNasabah.js
@@ -4,6 +4,7 @@ import { Button } from "primereact/button";
 import { Card } from "primereact/card";
 import { url } from '../../../Constanta';
 import { Dropdown } from 'primereact/dropdown';
+import { Link } from "react-router-dom";
 class InputNasabah extends Component {
   constructor(props) {
     super(props);
@@ -78,6 +79,7 @@ class InputNasabah extends Component {
 
     if (response.ok) {
       // this.props.history.push("/tlpadm/mst_plg");
+      alert("Data berhasil di Tambah");
       console.log('masuk')
     } else {
       console.error("Failed to add or edit customer data");
@@ -91,109 +93,119 @@ class InputNasabah extends Component {
     return (
       <Card>
         <form onSubmit={this.handleSubmit}>
-          <div className="flex">
-            <div className="field grid">
-              <label className="col-12 mb-2 md:col-2 md:mb-0" style={{ marginRight: '2%' }} htmlFor="idTransaksiNasabah">ID <span style={{ color: 'red' }}>*</span></label>
-              <InputText
-                id="idTransaksiNasabah"
-                name="idTransaksiNasabah"
-                value={idTransaksiNasabah}
-                onChange={this.handleInputChange}
-                required
-                aria-describedby="idPelanggan-help"
-                type="text"
-                className="col-12 md:col-10"
-              />
-              {/* <small id="idPelanggan-help">
-                ID Required.
-              </small> */}
-            </div>
-            <br />
-            <br />
-            <div className="field grid">
-              <label htmlFor="nama">Norek di Tuju</label>
-              <InputText
-                id="norekDituju"
-                name="norekDituju"
-                value={norekDituju}
-                onChange={this.handleInputChange}
-                required
-              // aria-describedby="idPelanggan-help"
-              // type="text"
-              // className="col-12 md:col-10"
-              />
-              {/* <small id="idPelanggan-help">
-                ID Required.
-              </small> */}
-            </div>
-            <br />
-            <div className="p-field">
-              <label className="col-12 mb-2 md:col-2 md:mb-0" htmlFor="norek">Nama <span style={{ color: 'red' }}>*</span></label>
-              <Dropdown
-                className="col-12 md:col-10 w-1"
-                name="norek"
-                value={norek}
-                onChange={this.handleInputChange}
-                options={this.state.listPelanggan}
-                optionLabel="nama"
-                placeholder="Select Nama"
-                style={{ width: '30%' }}
-              />
-            </div>
-            <br />
-            <div className="p-field">
-              <label htmlFor="noTelp">No Telepon</label>
-              <InputText
-                id="noTelp"
-                name="noTelp"
-                value={noTelp}
-                onChange={this.handleInputChange}
-                required
-              />
-            </div>
-            <br />
-            <div className="p-field">
-              <label htmlFor="uang">Uang</label>
-              <InputText
-                id="uang"
-                name="uang"
-                value={uang}
-                onChange={this.handleInputChange}
-                required
-              />
-            </div>
-            <br />
-            <div className="p-field">
-              <label htmlFor="status">Status</label>
-              <InputText
-                id="status"
-                name="status"
-                value={status}
-                onChange={this.handleInputChange}
-                required
-              />
-            </div>
-            <br />
-            <div className="p-field">
-              <label htmlFor="noTelp">Keterangan</label>
-              <InputText
-                id="statusKet"
-                name="statusKet"
-                value={statusKet}
-                onChange={this.handleInputChange}
-                required
-              />
-            </div>
-            <br />
-            <div className="p-field">
-              <label htmlFor="tanggal">Tanggal</label>
-              <input name="tanggal" type="date"
-                value={tanggal}
-                className="form-control" onChange={this.handleInputChange} />
-            </div>
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="idTransaksiNasabah">ID <span style={{ color: 'red' }}>*</span></label>
+            <InputText
+              className="col-11 md:col-10"
+              id="idTransaksiNasabah"
+              name="idTransaksiNasabah"
+              value={idTransaksiNasabah}
+              onChange={this.handleInputChange}
+              required
+              style={{ width: '10%' }}
+              type="text"
+            />
+          </div>
+          <br />
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="norekDituju">No Rek Dituju</label>
+            <InputText
+              className="col-11 md:col-10"
+              id="norekDituju"
+              name="norekDituju"
+              value={norekDituju}
+              onChange={this.handleInputChange}
+              required
+            // style={{ width: '30%'}}
+            />
+          </div>
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="norek">Nama</label>
+            <Dropdown
+              className="col-11 md:col-10"
+              name="norek"
+              value={norek}
+              onChange={this.handleInputChange}
+              options={this.state.listPelanggan}
+              optionLabel="nama"
+              placeholder="Select Nama"
+            />
+          </div>
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="noTelp">No Tlp</label>
+            <InputText
+              className="col-11 md:col-10"
+              id="noTelp"
+              name="noTelp"
+              value={noTelp}
+              onChange={this.handleInputChange}
+              required
+            // style={{ width: '30%'}}
+            />
+          </div>
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="uang">Uang</label>
+            <InputText
+              className="col-11 md:col-10"
+              id="uang"
+              name="uang"
+              value={uang}
+              onChange={this.handleInputChange}
+              required
+            // style={{ width: '30%'}}
+            />
+          </div>
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="status">Status</label>
+            <InputText
+              className="col-11 md:col-10"
+              id="status"
+              name="status"
+              value={status}
+              onChange={this.handleInputChange}
+              required
+            // style={{ width: '30%'}}
+            />
+          </div>
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="statusKet">Keterangan</label>
+            <InputText
+              className="col-11 md:col-10"
+              id="statusKet"
+              name="statusKet"
+              value={statusKet}
+              onChange={this.handleInputChange}
+              required
+            // style={{ width: '30%'}}
+            />
+          </div>
+          <br />
+          <div className="grid">
+            <label className="col-1 mb-2 md:col-2 md:mb-0" htmlFor="tanggal">Tanggal</label>
+            <InputText
+              className="col-11 md:col-10"
+              id="tanggal"
+              name="tanggal"
+              value={tanggal}
+              onChange={this.handleInputChange}
+              required
+            // style={{ width: '30%'}}
+            />
           </div>
 
-          <Button label={buttonText} type="submit" />
+
+          <div className="flex mt-4 justify-content-end">
+            <Button className="flex button-save" label={buttonText} type="submit" />
+            <Link to='/bnk_adm/trksi_nsbh/'>
+              <Button className="flex button-save" severity="secondary" label="Kembali" />
+            </Link>
+          </div>
         </form>
       </Card>
     );
